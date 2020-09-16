87f61dad52f7f7fa1c525fa7256bea5d02a76d76
Seokju Hong <iamseokjuhong@gmail.com>
2020-09-16 10:34:19 +0900

    회원가입 페이지 placeholder 삭제 및 한글화

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,11 +8,6 @@ User = get_user_model()
 
 
 class UserCreateForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': '아이디를 입력해주세요.'})
-        self.fields['password1'].widget.attrs.update({'placeholder': '비밀번호를 입력해주세요.'})
-        self.fields['password2'].widget.attrs.update({'placeholder': '비밀번호를 다시 입력해주세요.'})
 
     class Meta:
         model = User
@@ -24,6 +19,3 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["nickname"]
-        widgets = {
-            'nickname': forms.TextInput(attrs={'placeholder': '닉네임을 입력해주세요.'})
-        }
