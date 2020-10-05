419f5a0668f4b47233c8598cb154d7ad7169c00a
Seokju Hong <iamseokjuhong@gmail.com>
2020-10-05 14:56:27 +0900

    ajax 수정 구현

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -22,7 +22,7 @@ $(function () {
                 })
                 $.ajax({
                     type: "POST",
-                    url: myGlobal.url,
+                    url: myGlobal.changeUrl,
                     data: {
                         'csrfmiddlewaretoken': myGlobal.csrfmiddlewaretoken,
                         'pk': pk,
@@ -41,4 +41,70 @@ $(function () {
         }
     });
     $(".mv_zone .item-area").disableSelection();
+
+        $(document).on('click', '.update', function(e){
+        e.preventDefault();
+        var target = e.target;
+        var origin = $(target).parent();
+        var originClone = $(origin).children().clone();
+        var originTitle = $(origin).children('.title')[0].innerText;
+        var originContent = $(origin).children('.content')[0].innerText;
+        console.log(origin, originClone, originTitle, originContent)
+        /*
+        $.ajax({
+            type: 'GET',
+            url: `${$(target).attr('href')}`,
+            success: function (response) {
+                console.log(response)
+            }
+        })*/
+        var html = `
+            <form action="" method="POST" class="submit_form">
+                <input type="hidden" name="csrfmiddlewaretoken" value="${myGlobal.csrfmiddlewaretoken}">
+                <div class="contents">
+                    <div class="title RS_IP">
+                        <label for="id_title" class="title_text">제목</label>
+                        <input type="text" name="title" value="${originTitle}" maxlength="30" required id="id_title">
+                    </div>
+                    <div class="contents">
+                        <label for="id_content" class=" contents_text">할 일</label>
+                        <textarea name="content" cols="40" rows="10" id="id_content">${originContent}</textarea>
+                    </div>
+                </div>
+                <div class="toDoList_Check">
+
+                    <div class="is_progressed">
+                        <label for="id_is_progressed">진행 중</label>
+                        <input type="checkbox" name="is_progressed" id="id_is_progressed">
+                        <br />
+                        <label for="id_is_completed">완료</label>
+                        <input type="checkbox" name="is_completed" id="id_is_completed">
+                    </div>
+
+                </div>
+                <a href="javascript:void(0)" class="btn btn-outline-primary btn_cancel">취소</a>
+                <input type="submit" value="작성">
+            </form>`;
+
+        $(origin).empty().append(html);
+        $(document).on('click', '[class*=btn_cancel]', function () {
+            $(origin).empty().append(originClone);
+        });
+        $(document).on('submit', '.submit_form', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            var target = e.target;
+            var id = $(target).parents('.item-area').data('no');
+            var formData = $(this).serialize();
+
+            $.ajax({
+                type: "POST",
+                url: `${myGlobal.url}update/${id}/`,
+                data: formData,
+                success: function (response) {
+                    $("html").html(response);
+                }
+            });
+        });
+    });
 });
